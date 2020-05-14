930c992ea371c46b908934d9911226b3e6506cad
Seltzer-Software-Services-LLC <64972986+Seltzer-Software-Services-LLC@users.noreply.github.com>
2020-05-14 16:38:22 -0400

    Add files via upload

--- APIs/Walker Tracker/Python/create_walkertracker_config.py
+++ APIs/Walker Tracker/Python/create_walkertracker_config.py
@@ -3,7 +3,7 @@ config = configparser.ConfigParser()
 config['WalkerTracker']={}
 config['WalkerTracker']['token']='xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 config['WalkerTracker']['api_root'] = 'https:////api.walkertracker.com//api//2.1//'
-config['WalkerTracker']['GetCompetitorsStats'] = 'admin_competition_data'
+config['WalkerTracker']['get_competitors_stats'] = 'admin_competition_data'
 config['WalkerTracker']['competition_id'] = '55966'
 config['WalkerTracker']['cookie'] = 'PROXYSESSID=https:////10.100.101.95:443'
 with open('WalkerTracker.ini','w') as configfile:
