15c9946454fa71bad7f8073e96a8a3edb4f43068
Seltzer-Software-Services-LLC <64972986+Seltzer-Software-Services-LLC@users.noreply.github.com>
2020-05-14 16:31:21 -0400

    Update create_walkertracker_config.py

--- APIs/Walker Tracker/Python/create_walkertracker_config.py
+++ APIs/Walker Tracker/Python/create_walkertracker_config.py
@@ -5,6 +5,6 @@ config['WalkerTracker']['token']='xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 config['WalkerTracker']['api_root'] = 'https:////api.walkertracker.com//api//2.1//'
 config['WalkerTracker']['GetCompetitorsStats'] = 'admin_competition_data'
 config['WalkerTracker']['competition_id'] = '55966'
-config['WalkerTracer']['cookie'] = 'PROXYSESSID=https:////10.100.101.95:443'
+config['WalkerTracker']['cookie'] = 'PROXYSESSID=https:////10.100.101.95:443'
 with open('WalkerTracker.ini','w') as configfile:
     config.write(configfile)
